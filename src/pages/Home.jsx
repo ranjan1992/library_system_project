ff2b3392c4ddc8d22de6120d721af9cfee0ad467
Ranjan Verma <verma.ranjan62@gmail.com>
2026-02-09 13:33:55 +0530

    Adding the Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,14 @@
+import { Link } from "react-router-dom";
+import "./css/Home.css";
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="container">
+      <h1>Welcome to Online Library</h1>
+      <div className="categories">
+        <Link to="/books/fiction">Fiction</Link>
+        <Link to="/books/non-fiction">Non-Fiction</Link>
+        <Link to="/books/sci-fi">Sci-Fi</Link>
+      </div>
+    </div>
+  );
 }
