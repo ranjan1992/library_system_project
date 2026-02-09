cf5c2267e3d5d7ab5425ed9c224941b5eae44f34
Ranjan Verma <verma.ranjan62@gmail.com>
2026-02-09 13:54:46 +0530

    Added Adding Books Page with Form and CSS

--- src/pages/AddBook.jsx
+++ src/pages/AddBook.jsx
@@ -1,3 +1,45 @@
+import { useDispatch } from "react-redux";
+import { addBook } from "../redux/bookSlice";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import "./css/AddBook.css";
+
 export default function AddBook() {
-  return <div>AddBook</div>;
+  const [form, setForm] = useState({
+    title: "",
+    author: "",
+    category: "",
+    description: "",
+    rating: "",
+    image: "",
+  });
+  const dispatch = useDispatch();
+  const nav = useNavigate();
+
+  const submit = (e) => {
+    e.preventDefault();
+
+    // Checking if all fields are filled or not
+    if (!Object.values(form).every(Boolean)) return alert("Fill all fields");
+
+    // dispatching the addBook functionality present inside the bookSlice of the store
+    dispatch(addBook({ ...form, id: Date.now().toString() }));
+
+    // Navigating back to the Browse Books Page
+    nav("/books/all");
+  };
+
+  return (
+    <form onSubmit={submit} className="container--add">
+      <h1>Add Books</h1>
+      {Object.keys(form).map((k) => (
+        <input
+          key={k}
+          placeholder={k}
+          onChange={(e) => setForm({ ...form, [k]: e.target.value })}
+        />
+      ))}
+      <button>Add Book</button>
+    </form>
+  );
 }
