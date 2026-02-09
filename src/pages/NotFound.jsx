40f927b64205792cb81e7a1212c3235220b803ee
Ranjan Verma <verma.ranjan62@gmail.com>
2026-02-09 13:58:12 +0530

    Adding NotFound Page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,3 +1,14 @@
+import { Link } from "react-router-dom";
+import "./css/NotFound.css";
+
 export default function NotFound() {
-  return <div>NotFound</div>;
+  return (
+    <div className="container--not">
+      <div className="container--not_page">
+        <h2>404 Page Not Found !</h2>
+
+        <Link to="/">Go Home</Link>
+      </div>
+    </div>
+  );
 }
